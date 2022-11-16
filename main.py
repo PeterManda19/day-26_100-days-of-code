0d0fe36187d770ca400f90ad236101a9d1d69193
Peter Manda <109303643+PeterManda19@users.noreply.github.com>
2022-11-16 13:05:56 +0000

    Music player

--- main.py
+++ main.py
@@ -8,47 +8,59 @@ print("Press 1 to Play")
 print("Press 2 to Exit")
 print()
 print("Press anything else to see the menu again.")
+time.sleep(3)
 
 
 def play():
   source = audio.play_file('audio.wav')
   source.paused = False # unpause the playback
+ 
+
+def pause():
+  source = audio.play_file('audio.wav')
+  source.paused = True # pause the playback
+ 
+        
+def start():
   while True:
-    # Start taking user input and doing something with it
+    # clear the screen 
+    os.system("clear")
+  
+    # Show the menu
     input()
-
-while True:
-  # clear the screen 
-  os.system("clear")
-
-  # Show the menu
-
-  # take user's input
-
-  # check whether you should call the play() subroutine depending on user's input
-
-def quit():
-  exit = ""
-  while exit.lower() != "no" or exit.lower() != "n":  
-    exit = input("Do you want to generate another health stat? ") 
-    if exit.lower() == "yes" or exit.lower() == "y" or exit =="":
-      name = promptUser()
-      dice_6_8() 
-    else:
-     print()
-     print("Shutting down ⚔️ Character Stats Generator...")
-     break
-      
+    print("🎵 MyPOD Music Player")
+    print()
+    print("Press 1 to Play")
+    print("Press 2 to Exit")
+    print()
+   
+    # take user's input
+    try:
+      num = int(input(" "))
+      print()
+    except ValueError:
+      print()
+      print("I am expecting positive numbers.")
+      print()
+      continue
+    # check whether you should call the play() subroutine depending on user's input
+    if num == 1:
+      play()
+    elif num == 2:
+      pause()
+      break
+    
 
 def endGame():
   while True:
     print()
     input("""Thank you for playing!
-To play again please click Stop on top right page and click Run to generate health stats.""")
+To play again please click Stop on top right page and click Run to play music again.""")
     print()
     continue
 
 
 if __name__=="__main__":
-  
+  start()
+  pause()
   endGame()
